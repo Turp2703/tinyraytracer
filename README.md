1752e3e04f6aafd6b191a080ae36472df86ffd23
Tomás Rueda <turp2703@gmail.com>
2024-02-13 01:52:07 -0300

    ss fix

--- README.md
+++ README.md
@@ -3,6 +3,6 @@
 
 [Original SSLOY's tinyraytracer](https://github.com/ssloy/tinyraytracer)
 
-![Screenshot1](ss1.png)
-![Screenshot2](ss2.png)
-![Gif1](ss3.gif)
+![Screenshot1](ss/ss1.png)
+![Screenshot2](ss/ss2.png)
+![Gif1](ss/ss3.gif)
--- build/_deps/raylib-build/raylib/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/Programacion/Raylib/tinyraytracer/build/_deps/raylib-src/src
-# Build directory: D:/Programacion/Raylib/tinyraytracer/build/_deps/raylib-build/raylib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("external/glfw")
